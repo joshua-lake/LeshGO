a9d50fb8945cb71be229ff4cb64a7bb623d82dcd
James <james010@live.com>
2021-07-15 21:01:58 +1200

    fixed typo

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import MapViewDirections from 'react-native-maps-directions'
 import { Dimensions, StyleSheet, Text, View } from 'react-native'
 
 const origin = { latitude: -36.874935, longitude: 174.748596 }
-const destination = { latitude: -36.864675, longitude: 174.776329 }
-const GOOGLE_MAPS_APIKEY = 'AIzaSyCZdxO0PKO0pHQZOxD5zqAA4KcwPi1ypSQ' // bosh's api key
+const destination = { latitude: -36.9040097, longitude: 174.760000 }
+const GOOGLE_MAPS_APIKEY = 'AIzaSyCZdxO0PKO0pHQZOxD5zqAA4KcwPi1ypSQ' // bosh's api key -- actually james'...
 
 const App = () => {
   return (
