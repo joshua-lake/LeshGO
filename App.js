50786e174b1d06c0b88651a6063bfd2aa5152ef9
bosh-code <ryan.julian.bosher@icloud.com>
2021-07-15 20:45:33 +1200

    add demo api with route

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapViewDirections from 'react-native-maps-directions'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
 
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+const origin = { latitude: -36.874935, longitude: 174.748596 }
+const destination = { latitude: -36.864675, longitude: 174.776329 }
+const GOOGLE_MAPS_APIKEY = 'AIzaSyCZdxO0PKO0pHQZOxD5zqAA4KcwPi1ypSQ' // bosh's api key
 
-
-const logo = require('./assets/logo.png')
-
-export default function App () {
+const App = () => {
   return (
-    <>
-      <MapView
-        style={{ flex: 1 }}
-        provider={PROVIDER_GOOGLE}
-        showsUserLocation/>
-
-      <View style={styles.container}>
-        <Text>sheeeeeeeeesh</Text>
-        <Image source={logo} style={styles.logoImage}/>
-        <StatusBar style="auto"/>
-      </View>
-    </>
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Demo route</Text>
+      <MapView provider={PROVIDER_GOOGLE} style={styles.map} initialRegion={{
+        latitude: -36.872036,
+        longitude: 174.763428,
+        latitudeDelta: 0.0722,
+        longitudeDelta: 0.0421,
+      }}>
+        <MapView.Marker coordinate={origin} />
+        <MapView.Marker coordinate={destination} />
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAPS_APIKEY}
+          showsUserLocation
+          strokeWidth={3}
+          strokeColor="green"/>
+      </MapView>
+      <StatusBar style="auto"/>
+    </View>
   )
 }
 
@@ -32,8 +41,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  logoImage: {
-    width: '100%',
-    height: '50%',
+  map: {
+    width: Dimensions.get('window').width - 20,
+    height: Dimensions.get('window').height / 1.5,
+    borderRadius: 20,
   },
+
+  titleText: {
+    marginTop: 16,
+    paddingVertical: 8,
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
 })
+
+export default App
