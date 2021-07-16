4e1729def83ef5d1f52f191df503c2edd5ed4e39
James <james010@live.com>
2021-07-16 12:29:27 +1200

    refresh map route on destination change

--- src/components/maps.js
+++ src/components/maps.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect }from 'react'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 import MapViewDirections from 'react-native-maps-directions'
 import { Dimensions, StyleSheet } from 'react-native'
 
 
 const origin = { latitude: -36.874935, longitude: 174.748596 }
-const destination = { latitude: -36.9040097, longitude: 174.760000 }
+const incomingDestination = "3 cooper st grey lynn"
+// { latitude: -36.9040097, longitude: 174.710000 }
+const transportMode = "WALKING" // DRIVING BICYCLING TRANSIT WALKING
 const GOOGLE_MAPS_APIKEY = 'AIzaSyCZdxO0PKO0pHQZOxD5zqAA4KcwPi1ypSQ' // bosh's api key -- actually james'...
 
+
+
 const Maps = () => {
+  const [mapApiData, setMapAPIData] = useState()
+  const [destination, setDestination] = useState('')
+
+  useEffect(() => {
+    setDestination(incomingDestination)
+  }, [])
+
+  
+
+
   return (
 
       <MapView provider={PROVIDER_GOOGLE} style={styles.map} initialRegion={{
@@ -18,14 +32,18 @@ const Maps = () => {
         longitudeDelta: 0.0421,
       }}>
         <MapView.Marker coordinate={origin} />
-        <MapView.Marker coordinate={destination} />
+        {/* <MapView.Marker coordinate={destination} /> */}
         <MapViewDirections
           origin={origin}
           destination={destination}
           apikey={GOOGLE_MAPS_APIKEY}
+          mode={transportMode}
+          // alternatives="true"
+          timePrecision="now"
           showsUserLocation
           strokeWidth={3}
           strokeColor="green"/>
+        
       </MapView>
 
   )
