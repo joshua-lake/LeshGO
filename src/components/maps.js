244dfe822025f5bcb2bbf3c1e267acd4766c326c
James <james010@live.com>
2021-07-16 14:18:22 +1200

    added comments

--- src/components/maps.js
+++ src/components/maps.js
@@ -5,9 +5,8 @@ import { Dimensions, StyleSheet } from 'react-native'
 
 
 const HARDorigin = { latitude: -36.874935, longitude: 174.748596 }
-const incomingDestination = "8 morgan st newmarket"
-// { latitude: -36.9040097, longitude: 174.710000 }
-const transportMode = "DRIVING" // DRIVING BICYCLING TRANSIT WALKING
+const incomingDestination = "10 Marlborough street, mt eden" //  "9 gardner rd epsom" 8 morgan st newmarket { latitude: -36.9040097, longitude: 174.710000 }
+const transportMode = "WALKING" // DRIVING BICYCLING TRANSIT WALKING
 const GOOGLE_MAPS_APIKEY = 'AIzaSyCZdxO0PKO0pHQZOxD5zqAA4KcwPi1ypSQ' // bosh's api key -- actually james'...
 
 
@@ -20,23 +19,22 @@ const Maps = () => {
     setDestination(incomingDestination)
   }, [])
 
-  function setIncomingRouteState (data) {
+  function setIncomingRouteData (data) {
+    // console.log(data)
     const lastCoord = data.coordinates.length - 1
     setRouteData({
       returnedOrigin: data.coordinates[0], // lat long object
-      returnedDestination: data.coordinates[lastCoord],
+      returnedDestination: data.coordinates[lastCoord], // lat long object
       distanceKM: data.distance,
       durationMIN: data.duration
     })
   }
   // console.log(mapRouteData);
 
-  
-
 
   return (
 
-      <MapView provider={PROVIDER_GOOGLE} style={styles.map}  initialRegion={{
+      <MapView provider={PROVIDER_GOOGLE} style={styles.map} initialRegion={{ // showsTraffic="true"
         latitude: -36.872036,
         longitude: 174.763428,
         latitudeDelta: 0.0722,
@@ -44,16 +42,16 @@ const Maps = () => {
       }}>
         <MapView.Marker coordinate={mapRouteData.returnedOrigin} />
         <MapView.Marker coordinate={mapRouteData.returnedDestination} />
-        <MapViewDirections onReady={result => setIncomingRouteState(result)}
+        <MapViewDirections onReady={result => setIncomingRouteData(result)}
           origin={HARDorigin}
           destination={destination}
           apikey={GOOGLE_MAPS_APIKEY}
           mode={transportMode}
-          // alternatives="true"
           timePrecision="now"
           showsUserLocation
           strokeWidth={3}
-          strokeColor="green"/>
+          // strokeColor="green"
+          />
         
       </MapView>
 
