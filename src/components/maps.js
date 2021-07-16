a020b7110690a6f14744ddca779bbbcaf015e032
James <james010@live.com>
2021-07-16 13:05:56 +1200

    capture returning route data in state

--- src/components/maps.js
+++ src/components/maps.js
@@ -4,37 +4,48 @@ import MapViewDirections from 'react-native-maps-directions'
 import { Dimensions, StyleSheet } from 'react-native'
 
 
-const origin = { latitude: -36.874935, longitude: 174.748596 }
-const incomingDestination = "3 cooper st grey lynn"
+const HARDorigin = { latitude: -36.874935, longitude: 174.748596 }
+const incomingDestination = "8 morgan st newmarket"
 // { latitude: -36.9040097, longitude: 174.710000 }
-const transportMode = "WALKING" // DRIVING BICYCLING TRANSIT WALKING
+const transportMode = "DRIVING" // DRIVING BICYCLING TRANSIT WALKING
 const GOOGLE_MAPS_APIKEY = 'AIzaSyCZdxO0PKO0pHQZOxD5zqAA4KcwPi1ypSQ' // bosh's api key -- actually james'...
 
 
 
 const Maps = () => {
-  const [mapApiData, setMapAPIData] = useState()
+  const [mapRouteData, setRouteData] = useState({})
   const [destination, setDestination] = useState('')
 
   useEffect(() => {
     setDestination(incomingDestination)
   }, [])
 
+  function setIncomingRouteState (data) {
+    const lastCoord = data.coordinates.length - 1
+    setRouteData({
+      returnedOrigin: data.coordinates[0], // lat long object
+      returnedDestination: data.coordinates[lastCoord],
+      distanceKM: data.distance,
+      durationMIN: data.duration
+    })
+  }
+  // console.log(mapRouteData);
+
   
 
 
   return (
 
-      <MapView provider={PROVIDER_GOOGLE} style={styles.map} initialRegion={{
+      <MapView provider={PROVIDER_GOOGLE} style={styles.map}  initialRegion={{
         latitude: -36.872036,
         longitude: 174.763428,
         latitudeDelta: 0.0722,
         longitudeDelta: 0.0421,
       }}>
-        <MapView.Marker coordinate={origin} />
-        {/* <MapView.Marker coordinate={destination} /> */}
-        <MapViewDirections
-          origin={origin}
+        <MapView.Marker coordinate={mapRouteData.returnedOrigin} />
+        <MapView.Marker coordinate={mapRouteData.returnedDestination} />
+        <MapViewDirections onReady={result => setIncomingRouteState(result)}
+          origin={HARDorigin}
           destination={destination}
           apikey={GOOGLE_MAPS_APIKEY}
           mode={transportMode}
