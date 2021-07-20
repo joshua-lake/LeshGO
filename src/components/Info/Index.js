8be87ed9a09a81c22a44fc2ecba2ddce7d863423
James <james010@live.com>
2021-07-20 15:40:05 +1200

    write draft info page

--- src/components/Info/Index.js
+++ src/components/Info/Index.js
@@ -10,14 +10,18 @@ function Info (props) {
             <StyledText>
                 Car Emission data:
                 <GreyText>
-                    car co2 emission information is calculate based on a &quot;2021 fuel consumption Ratings&quot; dataset of 
-                    
+                    car Co2 emission information is calculated based on a &quot;2021 Fuel Consumption Ratings&quot; dataset of over 
+                    900 vehicles, which includes their grams of tailpipe Co2 emmision per kilometre based on average fuel consumption
+                    in both city and highway driving scenarios. This data has been provided to the public by the canadian government.
                 </GreyText>
             </StyledText>
             <StyledText>
                 Public transport data:
                 <GreyText>
-                    car emmision data calculate based on
+                    Co2 emissions of public transport routes is derived based on emmisions information from the Alexander Dennis Enviro200XLB,
+                    one of the most common bus models used throughout New Zealand and Auckland&apos;s public transport networks. While this vehicle doesnt apply to many public 
+                    transport routes, it does give us the most applicable statistics for the data we have access to, Which is then divided by 
+                    its average capacity to give a per person metric. 
                 </GreyText>
             </StyledText>
         </>
