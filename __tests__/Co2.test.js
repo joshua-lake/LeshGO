daece8b65bd0b80315f3fe5c2b4c3aad29075b9b
James <james010@live.com>
2021-07-21 11:21:08 +1200

    inital commit

---
+++ __tests__/Co2.test.js
@@ -0,0 +1,8 @@
+import { shallow } from 'enzyme'
+import { isTaskDefined } from 'expo-task-manager'
+import React from 'react'
+import { Platform, StyleSheet, View } from 'react-native'
+
+it(`Co2 for specific vehicle returns correct data`, () => {
+    const
+})
