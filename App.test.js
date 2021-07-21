bc4245adacd5ece5cc14b0515e4c71c629eec76d
bosh-code <ryan.julian.bosher@icloud.com>
2021-07-21 14:52:09 +1200

    working on map testing

--- App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react-native'
-import Info from './src/components/Info'
-
-test('examples of some things', async () => {
-  const { getByTestId, getByText, queryByTestId, toJSON } = render(<Info/>)
-  expect(1).toBe(1)
-})
