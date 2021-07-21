84b012540e0abd9f3cbd8728ea53a13186681061
bosh-code <ryan.julian.bosher@icloud.com>
2021-07-21 14:55:20 +1200

    merge and add info test

--- __tests__/Maps.test.js
+++ __tests__/Maps.test.js
@@ -1,17 +1,6 @@
 import { getDistance } from '../src/components/Maps/helper'
-import React from 'react'
-import { render } from '@testing-library/react-native'
-import Info from '../src/components/Info'
 
 it('returns the correct distance between two points', () => {
   const distance = getDistance(-36.8509, 174.7645, 37.7749, -122.4194)
   expect(distance).toBe(10507.66748764703)
-})
-
-/**
- * broken lmao
- */
-test('examples of some things', async () => {
-  const { getByText } = render(<Info/>)
-  expect(getByText(/900/)).toBeTruthy()
 })
